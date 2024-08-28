777d3c925376b342a4f0910f8b1d5d36c5ce9514
Esteban Correa <estebancorrea405@gmail.com>
2024-08-27 20:22:33 -0500

    Creando clase TipoDocumento y Rol

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,17 @@
 package org.example;
 
+import org.example.models.Rol;
+import org.example.models.TipoDocumento;
 import org.example.models.Usuario;
 
 public class Main {
     public static void main(String[] args) {
-        Usuario user1 = new Usuario();
-        user1.setId(1);
-        user1.setNombre("Sergio");
-        user1.setApellido("Prask");
-        user1.setId_tipodocumento(1);
-        user1.setNumerodocumento("455147664");
-        user1.setEmail("sergio@gmail.com");
-        user1.setDireccion("Parque heredia");
-        user1.setTelefono("3218416645");
-        user1.setId_rol(1);
-        user1.setEstado(true);
+        Rol rol = new Rol(1,"Cliente","CL");
+        TipoDocumento tipoDocumento = new TipoDocumento(1,"Cédula Ciudadanía","CC");
+        Usuario user = new Usuario(1,"Esteban","Correa",tipoDocumento.getId(), "1007256470","esteban18101@hotmail.com","Portales de San Fernando", "3017173082", rol.getId(), true);
 
-        System.out.println("User1: " + user1.getNombre() + " " + user1.getApellido());
-
-        Usuario user2 = new Usuario(1,"Esteban","Correa",1, "1007256470","esteban18101@hotmail.com","Portales de San Fernando", "3017173082", 1, true);
-        System.out.println("User2: " + user2.getNombre() + " " +user2.getApellido());
+        System.out.println("User: " + user.getNombre() + " " +user.getApellido());
+        System.out.println("Rol: " + rol.getName());
+        System.out.println("Tipo Documento: " + tipoDocumento.getName());
     }
 }
