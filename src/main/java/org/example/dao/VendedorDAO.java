bb37f650273b571c2c4befcf5bad923d869566d2
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-09-14 17:57:57 -0500

    [MOD] CRUD functionality into the VendedorDAO

--- src/main/java/org/example/dao/VendedorDAO.java
+++ src/main/java/org/example/dao/VendedorDAO.java
@@ -1,11 +1,13 @@
 package org.example.dao;
 
+import org.example.models.Cliente;
 import org.example.models.Usuario;
 import org.example.models.Vendedor;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,100 +19,201 @@ public class VendedorDAO implements InterfaceDAO {
     ResultSet rs = null;
     ArrayList<Usuario> listaVendedor = new ArrayList<>();
 
+
     @Override
-    public void insertar() throws SQLException {
-        Scanner in = new Scanner(System.in);
+    public void insertar() throws SQLException, ParseException {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar información del vendedor
+        System.out.println("Digite nombre de la persona: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Digite apellido de la persona: ");
+        String apellido = scanner.nextLine();
+        System.out.println("Digite el número de tipo de identificación: ");
+        System.out.println("1. Cédula ciudadania: ");
+        System.out.println("2. Cédula extranjeria: ");
+        System.out.println("3. Pasaporte: ");
+        int idTipoDocumento = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite el número de documento de identidad: ");
+        String numero = scanner.nextLine();
+        System.out.println("Digite el email: ");
+        String email = scanner.nextLine();
+        System.out.println("Digite la dirección: ");
+        String direccion = scanner.nextLine();
+        System.out.println("Digite el número de telefono: ");
+        String telefono = scanner.nextLine();
+        int idRol = 3;
+
+        String sql = "INSERT INTO Usuario(`nombre`, `apellido`, `id_tipodocumento`, `numerodocumento`, `email`, `direccion`, `telefono`, `registro`, `id_rol`, `estado`) VALUES (?,?,?,?,?,?,?,?,?,?)";
+
 
         // Date formatter to parse and format dates
         SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
 
+        System.out.println("Digite la fecha de contratación del vendedor (DD/MM/YYYY): ");
+        String fechaContratacionInput = scanner.nextLine();
+
+        // Parse the input date
+        Date fechaContratacionDate = inputFormat.parse(fechaContratacionInput);
+
+        // Format the date to match MySQL's expected format
+        String fechaContratacion = outputFormat.format(fechaContratacionDate);
+
         try {
-            System.out.println("Digite la fecha de contratación del vendedor (DD/MM/YYYY): ");
-            String fechaContratacionInput = in.nextLine();
+            // Preparar la sentencia SQL
+            pstm = conn.conexion().prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            pstm.setString(1, nombre);
+            pstm.setString(2, apellido);
+            pstm.setInt(3, idTipoDocumento);
+            pstm.setString(4, numero);
+            pstm.setString(5, email);
+            pstm.setString(6, direccion);
+            pstm.setString(7, telefono);
+            pstm.setDate(8, new java.sql.Date(System.currentTimeMillis()));
+            pstm.setInt(9, idRol);
+            pstm.setBoolean(10, true);
 
-            // Parse the input date
-            Date fechaContratacionDate = inputFormat.parse(fechaContratacionInput);
+            int affectedRows = pstm.executeUpdate();
 
-            // Format the date to match MySQL's expected format
-            String fechaContratacion = outputFormat.format(fechaContratacionDate);
 
-            System.out.println("Digite la comisión recibida: ");
-            int comision = in.nextInt();
+            if (affectedRows > 0) {
+                // Obtener el id_usuario generado automáticamente
+                ResultSet generatedKeys = pstm.getGeneratedKeys();
+                System.out.println("generatedKeys "+ generatedKeys);
+                if (generatedKeys.next()) {
+                    int id_usuario = generatedKeys.getInt(1); // El ID generado para el usuario
+                    System.out.println("Vendedor insertado con éxito en Usuario. ID de usuario generado: " + id_usuario);
 
-            System.out.println("Digite el id usuario: ");
-            int idUsuario = in.nextInt();
+                    // insertar el id_usuario en la tabla vendedor
+                    String sqlCliente = "INSERT INTO Vendedor(`fecha_contratacion`, `id_usuario`) VALUES(?,?)";
 
-            String sql = "INSERT INTO Vendedor(`fecha_contratacion`, `comision`, `id_usuario`) VALUES(?,?,?)";
-            PreparedStatement stm = conn.conexion().prepareStatement(sql);
-            stm.setString(1, fechaContratacion);
-            stm.setInt(2, comision);
-            stm.setInt(3, idUsuario);
+                    PreparedStatement pstmVendedor = conn.conexion().prepareStatement(sqlCliente);
+                    pstmVendedor.setString(1, fechaContratacion);
+                    pstmVendedor.setInt(2, id_usuario);
 
-            int rs = stm.executeUpdate();
+                    // Insertar en Vendedor
+                    int affectedRowsCliente = pstmVendedor.executeUpdate();
 
-            if (rs > 0) {
-                System.out.println("Registro insertado de forma correcta");
+                    if (affectedRowsCliente > 0) {
+                        System.out.println("Vendedor insertado con éxito en la tabla Cliente.");
+                    } else {
+                        System.out.println("Error al insertar en la tabla Vendedr.");
+                    }
+
+                    pstmVendedor.close();
+                }
             } else {
-                System.out.println("Fallo en la inserción");
+                System.out.println("Error al insertar el vendedor en Usuario.");
             }
-        }catch (Exception e) {
-            System.out.println("Error:" + e);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (pstm != null) {
+                pstm.close();
+            }
         }
     }
+
 
     @Override
     public void eliminar() throws SQLException {
         Scanner in=new Scanner(System.in);
 
-        System.out.print("Digite el id del vendedor a eliminar: ");
-        int id=in.nextInt();
+        listar();
 
-        String sql="DELETE FROM Vendedor WHERE id=?";
-        var stm = conn.conexion().prepareStatement(sql);
-        stm.setObject(1,id);
-        int rs=stm.executeUpdate();
+        if (listaVendedor.isEmpty()) {
+            return;
+        }
 
-        if(rs>0){
-            System.out.println("Registro eliminado");
+        System.out.print("Digite el id del cliente a eliminar: ");
+        int id_usuario=in.nextInt();
+
+        // Eliminar primero de la tabla Cliente
+        String sqlVendedor = "DELETE FROM Vendedor WHERE id_usuario = ?";
+        String sqlUsuario = "DELETE FROM Usuario WHERE id = ?";
+        try {
+            pstm = conn.conexion().prepareStatement(sqlVendedor);
+            pstm.setInt(1, id_usuario);
+
+            int affectedRows = pstm.executeUpdate();
+            if (affectedRows > 0) {
+                System.out.println("Vendedor eliminado con exito de la tabla Cliente.");
+
+                //Eliminar de la tabla usuario
+                pstm = conn.conexion().prepareStatement(sqlUsuario);
+                pstm.setInt(1, id_usuario);
+                int affectedRowsUsuario = pstm.executeUpdate();
+
+                if (affectedRowsUsuario > 0) {
+                    System.out.println("Usuario eliminado con exito de la tabla Usuario.");
+                }else{
+                    System.out.println("Error al eliminar en la tabla Usuario.");
+                }
+            }else {
+                System.out.println("Error al eliminar en la tabla Vendedor.");
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (pstm != null) {
+                pstm.close();
+            }
         }
-        else {
-            System.out.println("Eiminacion fallida");
-        }
-        in.close();
+
     }
 
     @Override
     public void modificar() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+        listar();
+
+        if (listaVendedor.isEmpty()) {
+            return;
+        }
+
+        System.out.println("Seleccione el ID del vendedor que desea modificar:");
+        int id_usuario = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Ingrese el nuevo nombre del vendedor:");
+        String nuevoNombre = scanner.nextLine();
+
+        System.out.println("Ingrese la nueva dirección del vendedor:");
+        String nuevaDireccion = scanner.nextLine();
+
+        // Sentencia SQL para hacer un UPDATE con un JOIN entre Usuario y Vendedor
+        String sql = "UPDATE Usuario u " +
+                "JOIN Vendedor v ON u.id = v.id_usuario " +
+                "SET u.nombre = ?, u.direccion = ? " +
+                "WHERE u.id = ?";
         try {
-            Scanner in = new Scanner(System.in);
+            pstm = conn.conexion().prepareStatement(sql);
+            pstm.setString(1, nuevoNombre);
+            pstm.setString(2, nuevaDireccion);
+            pstm.setInt(3, id_usuario);
 
-            System.out.println("Digite la nueva comisión del vendedor: ");
-            int comision = in.nextInt();
-            System.out.println("Digite el ID del vendedor a actualizar: ");
-            int id= in.nextInt();
+            int affectedRows = pstm.executeUpdate();
 
-            String sql="UPDATE Vendedor SET comision=? WHERE id=?";
-            var stm= conn.conexion().prepareStatement(sql);
-            stm.setObject(1, comision);
-            stm.setObject(2,id);
-            int rs = stm.executeUpdate();
-
-            if(rs>0){
-                System.out.println("Registro actualizado de forma correcta");
+            if (affectedRows > 0) {
+                System.out.println("Vendedor modificado con éxito.");
+            } else {
+                System.out.println("Error: No se encontró el vendedoor con el ID proporcionado.");
             }
-            else{
-                System.out.println("Fallo en la actualizacion");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (pstm != null) {
+                pstm.close();
             }
-            in.close();
-        } catch (Exception e) {
-            System.out.println("Error:" + e);
         }
     }
 
     @Override
     public void listar() throws SQLException {
-        String sql = "SELECT * FROM Vendedor";
+        String sql = "SELECT u.*, v.fecha_contratacion FROM Usuario u JOIN Vendedor v ON u.id = v.id_usuario WHERE u.id_rol= 3 AND u.estado = 1";
         pstm = conn.conexion().prepareStatement(sql);
         rs = pstm.executeQuery();
 
@@ -125,26 +228,76 @@ public class VendedorDAO implements InterfaceDAO {
 
     @Override
     public void buscar() throws SQLException {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Digite el id de usuario: ");
-        int idUsuario = in.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        String sql = "SELECT * FROM Vendedor where id_usuario=?";
+        System.out.println("Digite el nombre del vendedor: ");
+        String nombre = scanner.nextLine();
+        // SQL con JOIN y búsqueda por nombre
+        String sql = "SELECT u.*, v.fecha_contratacion FROM Usuario u JOIN Vendedor v ON u.id = v.id_usuario WHERE u.id_rol = 3 AND u.estado = 1 AND u.nombre LIKE ?";
+
         pstm = conn.conexion().prepareStatement(sql);
-        pstm.setObject(1, idUsuario);
+        pstm.setString(1, "%" + nombre + "%");
         rs = pstm.executeQuery();
 
+        listaVendedor.clear();
+
+        // Mostrar los vendedores encontrados
+        System.out.println("Vendedores encontrados:");
         while (rs.next()) {
-            Vendedor vendedor = Vendedor.load(rs);
+            Vendedor vendedor = new Vendedor();
+
+            vendedor.setNombre(rs.getString("nombre"));
+            vendedor.setApellido(rs.getString("apellido"));
+            vendedor.setEmail(rs.getString("email"));
+            vendedor.setDireccion(rs.getString("direccion"));
+            vendedor.setTelefono(rs.getString("telefono"));
+            vendedor.setFecha_contratacion(rs.getDate("fecha_contratacion"));
+
             listaVendedor.add(vendedor);
+
+            // Mostrar la información del vendedor
+            System.out.println("Nombre: " + vendedor.getNombre() + ", Apellido: "+ vendedor.getApellido() + ", Email: " + vendedor.getEmail() + ", Dirección: " + vendedor.getDireccion() + ", Teléfono: " + vendedor.getTelefono() + ", Fecha contratacion: " + vendedor.getFecha_contratacion());
         }
 
-        String usersFormatted = String.format("Vendedor: %s", listaVendedor);
-        System.out.println(usersFormatted);
+        if (listaVendedor.isEmpty()) {
+            System.out.println("No se encontraron clientes con ese nombre.");
+        }
     }
 
     @Override
     public void deshabilitar() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
 
+        listar();
+
+        if (listaVendedor.isEmpty()) {
+            return;
+        }
+
+        // Solicitar el ID del usuario a desactivar
+        System.out.println("Ingrese el ID del vendedor que desea desactivar:");
+        int id_usuario = scanner.nextInt();
+
+        // Actualizar el estado del usuario a 0 (inactivo) en la tabla Usuario
+        String sql = "UPDATE Usuario SET estado = 0 WHERE id = ? AND id_rol = 3";
+
+        try {
+            // Preparar y ejecutar la actualización
+            pstm = conn.conexion().prepareStatement(sql);
+            pstm.setInt(1, id_usuario);
+            int affectedRows = pstm.executeUpdate();
+
+            if (affectedRows > 0) {
+                System.out.println("Vendedor desactivado con éxito.");
+            } else {
+                System.out.println("Error al desactivar el vendedor.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (pstm != null) {
+                pstm.close();
+            }
+        }
     }
 }
