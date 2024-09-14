5feb2e131e2f070c840ab9f73d3db5295cb225d8
Yeizermarrugo <yeizermarrugo@gmail.com>
2024-09-14 18:30:46 -0500

    [Add] parseException into the insert method

--- src/main/java/org/example/dao/InterfaceDAO.java
+++ src/main/java/org/example/dao/InterfaceDAO.java
@@ -1,9 +1,10 @@
 package org.example.dao;
 
 import java.sql.SQLException;
+import java.text.ParseException;
 
 public interface InterfaceDAO {
-    public void insertar() throws SQLException;
+    public void insertar() throws SQLException, ParseException;
     public void eliminar() throws SQLException;
     public void modificar() throws SQLException;
     public void listar() throws SQLException;
