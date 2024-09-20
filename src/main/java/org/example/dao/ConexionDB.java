f1a633c9bcdcd2e96fbc3bf6faf665ae5797d2a4
Esteban Correa <estebancorrea405@gmail.com>
2024-09-19 20:30:20 -0500

    Documentando conexión a base de datos

--- src/main/java/org/example/dao/ConexionDB.java
+++ src/main/java/org/example/dao/ConexionDB.java
@@ -1,17 +1,27 @@
-package org.example.dao;
+package org.example.dao; // Declaramos que este código pertenece al paquete 'org.example.dao'.
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection; // Importamos la clase Connection para manejar conexiones a la base de datos.
+import java.sql.DriverManager; // Importamos la clase DriverManager para establecer conexiones a la base de datos.
+import java.sql.SQLException; // Importamos la clase SQLException para manejar errores de SQL.
 
-public class ConexionDB {
-    Connection con;
+public class ConexionDB { // Definimos la clase ConexionDB para gestionar la conexión a la base de datos.
+    Connection con; // Declara una variable de tipo Connection para almacenar la conexión.
+
+    // URL de conexión a la base de datos, incluye el tipo de base de datos, la ubicación y el nombre de la base de datos.
     private String dateBaseUrl = "jdbc:mysql://localhost:3306/vehicles_db";
-    private String usuario = "root";
-    private String clave = "";
 
+    // Credenciales para la conexión a la base de datos.
+    private String usuario = "root"; // Nombre de usuario de la base de datos.
+    private String clave = ""; // Contraseña de la base de datos.
+
+    /**
+     * Establece una conexión con la base de datos.
+     * @return un objeto Connection que representa la conexión a la base de datos.
+     * @throws SQLException Si ocurre un error al intentar establecer la conexión.
+     */
     public Connection conexion() throws SQLException {
+        // Intentamos establecer la conexión utilizando DriverManager.
         con = DriverManager.getConnection(this.dateBaseUrl, this.usuario, this.clave);
-        return con;
+        return con; // Retornamos la conexión establecida.
     }
 }
